ba924dfded603e0432f62364cf3a723312830cba
Mateusz Malinowski <m4linka@gmail.com>
2013-07-11 06:52:09 +0000

    reading from file index.html

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var filename = 'index.html';
+var mainPageBuffer = fs.readFileSync(filename);
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(mainPageBuffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
